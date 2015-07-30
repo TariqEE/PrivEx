57b3c87b9a613772626680b399d809851c4e5b35
Tariq <tariq.elahi@uwaterloo.ca>
2015-07-30 07:22:23 -0400

    Added the license file and updated the scripts a little.

--- util/find_average.py
+++ util/find_average.py
@@ -1,21 +1,25 @@
 import pprint
-
+bad_sites = []
 if __name__ == '__main__':
   results = {}
-  epochs = 0 
-  with open('results.txt','r') as f:
+  epochs = 0
+  with open('bad_share_sites.txt', 'r') as g:
+    for line in g:
+      bad_sites.append(line.strip())
+
+  with open('consol-results.txt','r') as f:
     for line in f:
-      site, visits = line.strip().split(":")
-      if site == 'Other':
-        epochs = epochs + 1
-      if site in results:
-        results[site] = float(visits) + results[site]
-      else:
-       results[site] = float(visits)
+      epoch, site, visits = line.strip().split(":")
+      if site not in bad_sites:
+        if site == 'Other':
+          epochs = epochs + 1
+        if site in results:
+          results[site] = float(visits) + results[site]
+        else:
+          results[site] = float(visits)
 
   for key in results:
     results[key] = results[key]/epochs
-#  pprint.pprint(results)
   print 'Epochs: ', epochs
-  for site in sorted(results, key=results.get, reverse=True):
+  for site in sorted(results, key=results.get, reverse=False):
     print site, results[site]
--- util/test_dist.py
+++ util/test_dist.py
@@ -2,27 +2,36 @@ import numpy as np
 import random
 import math
 import matplotlib.pyplot as plt
+import pprint as pprint
 
+websites = 6071
 sigma = 240
-epochs = 167
+epochs = 122
 random_samples = []
 privex_data = []
 
 if __name__ == '__main__':
-    sd = sigma/math.sqrt(epochs)
-    for i in range(0, 4000):
+    sd = (sigma/math.sqrt(epochs))
+    for i in range(0, websites):
         random_samples.append(random.gauss(0, sd))
     random_samples.sort()
-    sample_cdf_data = np.cumsum(random_samples)
+    X = np.array(random_samples)
+    pprint.pprint(X)
+    Y_range = np.linspace(0, len(random_samples), len(random_samples), endpoint=True)/len(random_samples)
+    plt.plot(X, Y_range, label='N(0,%f)' % sd)
 
-    with open('results_stats.txt', r) as f:
+    with open('results_stats.txt', 'r') as f:
         for line in f:
-            if "Other" not in line:
+            if "Other" not in line and 'Epochs' not in line and "Censored" not in line:
                 _, count = line.strip().split()
-                privex_data.append(count)
-    privex_data.sort()
-    privex_cdf = np.cumsum(privex_data)
+                privex_data.append(float(count))
 
-    plt.plot(sample_cdf_data, privex_cdf)
+    privex_X = np.array(privex_data)
+    privex_Y_range = np.linspace(0, len(privex_data), websites, endpoint=True)/len(privex_data)
+    print len(privex_X), len(privex_Y_range)
+    plt.plot(privex_X, privex_Y_range, label='Privex: %d Epoch(s)' % epochs)
+
+    plt.legend(loc='lower right')
+    plt.xlabel('Visits')
     plt.ylabel('CDF')
     plt.show()
